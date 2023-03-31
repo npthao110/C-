c0c82c6c1d61e5ae9fa4f69663b605c3daafd425
npthao110 <111837093+npthao110@users.noreply.github.com>
2023-03-30 20:42:13 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+
+namespace Lambda
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // (int a, int b) =>
+            // {
+            //     int kq = a + b;
+            //     return kq;
+            // }
+            // Action<string> thongbao;
+            // thongbao = (string s) => Console.WriteLine(s);
+
+            // for (int i = 0; i < 10; i++)
+            // {
+            //     thongbao?.Invoke("xin chao");
+            // }
+
+
+            // Action<string, string> welcome;
+            // welcome = (string msg, string name) => Console.WriteLine(msg + " " + name);
+            // welcome?.Invoke("xin chao", "cac ban");
+            // welcome?.Invoke("xinchao", "Nguyen van A");
+
+            Func<int, int, int> tinhtoan;
+
+            tinhtoan = (a, b) =>
+            {
+                int kq = a + b;
+                return kq;
+            };
+
+            Console.WriteLine(tinhtoan.Invoke(5, 6));
+
+
+        }
+    }
+}
+

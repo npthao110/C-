3a11ef7c72b16697e4b19138bae4d17a7776a116
npthao110 <111837093+npthao110@users.noreply.github.com>
2023-03-30 20:40:18 -0500

    Add files via upload

---
+++ Program .cs
@@ -0,0 +1,55 @@
+﻿using System;
+using static System.Console;
+
+namespace Delegate
+{
+    public delegate void ShowLog(string message);
+    class Program
+    {
+        static void Info(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(s);
+            Console.ResetColor();
+        }
+
+        static void Warning(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(s);
+            Console.ResetColor();
+        }
+
+        static int Tong(int a, int b) => a + b;
+        static void Main(string[] args)
+        {
+            // ShowLog log = null;
+
+            // log += Info;
+            // log += Info;
+            // log += Info;
+            // log += Warning;
+            // log += Warning;
+
+            // log?.Invoke("Xin chao");
+
+            // action
+
+            // Action action;
+            // Action<string> action2;
+
+            // action2 = Warning;
+            // action2 += Info;
+            // action2?.Invoke("Thong bao");
+            // Func<int> f1; //delegate int func()
+
+            Func<int, int, int> tinhtoan;
+            int a = 5;
+            int b = 10;
+
+            tinhtoan = Tong;
+            Console.WriteLine($"KQ:  {tinhtoan(a, b)}");
+        }
+    }
+}
+
